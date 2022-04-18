644e48331984e04334bd120cefef287e64b7489a
Execcl <74021950+Execcl@users.noreply.github.com>
2022-04-17 19:54:53 -0500

    initial commit

---
+++ game_table.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum class game_table
+{
+    r_endframe = 0x474880,
+    sys_io_cellpadgetdata = 0x081951C,
+
+    scrplaceview = 0xCD6670,
+    uicontext = 0x13DAD58,
+
+    r_addcmddrawstretchpic = 0x0472E78,
+    cg_drawrotatedpicphysical = 0x014C018,
+    r_addcmddrawtext = 0x0473658,
+    db_findxassetheader = 0x04DF4E8,
+    r_textwidth = 0x0459E80,
+
+    va = 0x38DC30
+};
